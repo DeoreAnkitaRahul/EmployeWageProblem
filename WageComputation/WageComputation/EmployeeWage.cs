1491cafde7e3f5d0304ed2946de2ad2883d71b67
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-04-25 14:26:23 +0530

    [Ankita] Add : UC4-Using Swaitch Case

--- WageComputation/WageComputation/EmployeeWage.cs
+++ WageComputation/WageComputation/EmployeeWage.cs
@@ -5,30 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace WageComputation
-{ 
-     public class EmployeeCheck
+{
+    public class EmployeeCheck
     {
-        public int IS_FULLTIME = 1;
-        public int EMP_RATE_PER_HOUR = 20;
-        public int IS_PART_TIME = 2;
+        public const int IS_FULLTIME = 1;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 2;
         public void CheckEmployee()
         {
             int empHrs = 0;
             int empWage = 0;
 
-             Random random = new Random();
+            Random random = new Random();
             int empCheck = random.Next(3);
 
-            if (empCheck == IS_FULLTIME)
-                empHrs = 8;
-
-            else if (empCheck == IS_PART_TIME)
-                empHrs = 4;
-            else
-                empHrs = 0;
-              empWage = empHrs * EMP_RATE_PER_HOUR;
+            switch (empCheck)
+            {
+                case IS_FULLTIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
+            }
+             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee wage is: " + empWage);
 
+
+
+
         }
-    }
-}
+}   }   
